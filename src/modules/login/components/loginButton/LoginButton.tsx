46b249fa5676d6a4c321d0e7e85e0804da429766
coposaja <yohanzen.alexander@gmail.com>
2020-08-01 10:23:04 +0700

    Add Login process information to UI

--- src/modules/login/components/loginButton/LoginButton.tsx
+++ src/modules/login/components/loginButton/LoginButton.tsx
@@ -1,11 +1,14 @@
 import React, { ButtonHTMLAttributes } from 'react'
 
 import classes from './LoginButton.module.css';
+import MaterialIcon from '../../../../components/materialIcon/MaterialIcon';
 
 const LoginButton: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
   return (
     <button className={classes.LoginButton} {...props}>
-      Login
+      {!props.disabled ? 'Login' :
+        <MaterialIcon iconName="cached" fontSize="1.25rem" className={["spinning"]} />
+      }
     </button>
   )
 }
