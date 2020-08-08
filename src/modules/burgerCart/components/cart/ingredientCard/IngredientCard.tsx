5452a53aca47331fb6ab6c118025837d3e1a168b
coposaja <yohanzen.alexander@gmail.com>
2020-08-08 11:44:15 +0700

    Add IngredientCard component

---
+++ src/modules/burgerCart/components/cart/ingredientCard/IngredientCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import classes from '../Cart.module.css';
+import { ingreidentDictionary } from '../../../../../constants';
+
+declare interface IIngredientCard {
+  ingredients: string
+}
+
+const IngredientCard = (props: IIngredientCard) => {
+  let ingredients: { [key: string]: number } = {};
+  props.ingredients.split('').map((ingredient) => {
+    ingredients[ingredient] ? ingredients[ingredient] += 1 : ingredients[ingredient] = 1;
+  })
+
+  return (
+    <div className={classes.IngredientCardWrapper}>
+      {Object.keys(ingredients).map((ingredient) => (
+        <div className={classes.IngredientCard}>
+          {ingreidentDictionary[ingredient] + (ingredients[ingredient] > 1 ? ` X${ingredients[ingredient]}` : '')}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default IngredientCard;
